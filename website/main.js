2bd5a7413794e0564968088f38b936e47743f041
Luong Van Linh <luongvanlinh12@gmail.com>
2024-10-06 10:07:28 +0700

    final project

--- website/main.js
+++ website/main.js
@@ -2,9 +2,7 @@
 const API_KEY = "&APPID=e23122c5062eb361eb2aa6ee3762e1db&units=imperial";
 const baseURL = "https://api.openweathermap.org/data/2.5/weather?q=";
 
-// Convert date
 function convertDate(unixtimestamp) {
-  // Months array
   var months_array = [
     "Jan",
     "Feb",
@@ -20,25 +18,15 @@ function convertDate(unixtimestamp) {
     "Dec",
   ];
 
-  // Convert timestamp to milliseconds
   var date = new Date(unixtimestamp * 1000);
-
-  // Year
   var year = date.getFullYear();
-
-  // Month
   var month = months_array[date.getMonth()];
-
-  // Day
   var day = date.getDate();
-
-  // Display date time in MM/dd/yyyy format
   var convertedTime = month + "/" + day + "/" + year;
 
   return convertedTime;
 }
 
-// Event listener to add function to existing HTML DOM element
 /* Function called by event listener */
 document.getElementById("generate").addEventListener("click", displayAction);
 
@@ -59,7 +47,6 @@ function displayAction() {
     .then(() => updateUI());
 }
 
-// Async GET
 /* Function to GET Web API Data*/
 const getDataApi = async (baseURL, zip, API_KEY) => {
   if (zip.toString().length !== 5) {
@@ -70,7 +57,6 @@ const getDataApi = async (baseURL, zip, API_KEY) => {
 
     const request = await fetch(url);
     try {
-      // Transform into JSON
       const allData = await request.json();
       if (allData.message) {
         alert(allData.message);
@@ -79,14 +65,10 @@ const getDataApi = async (baseURL, zip, API_KEY) => {
       }
     } catch (error) {
       console.log("error", error);
-      // appropriately handle the error
     }
   }
 };
 
-// Async POST
-/* Function to POST data */
-// Async POST
 const postDataApi = async (url = "", data = {}) => {
   console.log("post weather data: ", data);
   const response = await fetch(url, {
@@ -95,7 +77,7 @@ const postDataApi = async (url = "", data = {}) => {
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(data), // body data type must match "Content-Type" header
+    body: JSON.stringify(data),
   });
 
   try {
@@ -106,7 +88,6 @@ const postDataApi = async (url = "", data = {}) => {
   }
 };
 
-/* Function to update UI */
 const updateUI = async () => {
   const request = await fetch("/all");
   try {
